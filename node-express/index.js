431276eade62b1ebb0bbf4c38de3f72cd0307210
nabi-elhraichi <nabil.elhraichi@edu.umi.ac.ma>
2020-10-14 14:10:24 +0200

    Express generator

--- node-express/index.js
+++ node-express/index.js
@@ -4,6 +4,9 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const dishRouter = require('./routes/dishRouter');
+const promotionRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
+
 const hostname = 'localhost';
 const port = 3000;
 
@@ -12,6 +15,8 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 app.use('/dishes', dishRouter);
+app.use('/promotions', promotionRouter);
+app.use('/leaders', leaderRouter);
 
 app.use(express.static(__dirname+ '/public'));
 
