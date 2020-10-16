010f566231d73e7339adc696ae37cdfbc88d6fb2
nabi-elhraichi <nabil.elhraichi@edu.umi.ac.ma>
2020-10-16 15:16:28 +0200

    Mongoose Part 3

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -8,18 +8,31 @@ const connect = mongoose.connect(url);
 connect.then((db)=> {
     console.log('Connected correctly to the server');
 
-    var newDish = Dishes({
-        name: 'Uthappiza',
+    Dishes.create({
+        name: 'piza1',
         description : 'test'
-    });
-
-    newDish.save()
+    })
     .then((dish) => {
         console.log("added successfully \n", dish);
-        return Dishes.find({});
+        return Dishes.findByIdAndUpdate(dish._id,{
+            $set: { description: 'Updated test'}
+        },{
+            new: true
+        }).exec();
     })
-    .then( (dishes) => {
-        console.log("Found \n" , dishes);
+    .then( (dish) => {
+        console.log("Found \n" , dish);
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling !',
+            author: 'Nabil EL HRAICHI'
+        });
+
+
+        return dish.save();
+    })
+    .then((dish)=> {
+        console.log("Comments pushed \n" , dish);
         return Dishes.remove({});
     })
     .then(() => {
